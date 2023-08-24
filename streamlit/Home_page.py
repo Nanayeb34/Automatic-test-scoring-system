3ae1ef12ad1730fbd4cb6e67e80c295af7f9bd44
nanayeb34 <nanayeb34@gmail.com>
2023-08-24 15:41:41 +0000

    simplified UI

--- streamlit/Home_page.py
+++ streamlit/Home_page.py
@@ -3,10 +3,9 @@ import os
 import tempfile
 from PIL import Image
 import base64
-import pyarrow as pa
 import time
-from scoring_system import scan_without_dialog, reference_sheet, student_sheet, score
-import logging
+from scoring_system import student_sheet
+import pandas as pd
 favicon_path = os.path.join(os.getcwd(),'assets','black_favicon.ico')
 favicon=Image.open(favicon_path)
 st.set_page_config(
@@ -79,112 +78,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-# def show_warning_and_get_input(index_numbers,student_id):
-#     if f"corrected_index_input_{student_id}" not in st.session_state:
-#         st.session_state[f"corrected_index_input_{student_id}"]=[]
-#     if len(str(index_numbers)) != 7:
-#         st.warning("Number of index numbers is not equal to 7.", icon="⚠️")
-        
-#         # Display the reference image using matplotlib
-#         # pil_image=Image.open(image_path)
-#         # st.image(pil_image, use_column_width=True, caption="Uploaded Image")
-
-#         # Display current index numbers
-#         # st.write(f"Current index numbers: {index_numbers}")
-#         # stored_corrected_index = st.session_state.get(f"stored_corrected_index_{student_id}")
-#         # Get user input for the corrected index number
-#         corrected_index = st.text_input(
-#             "Enter correct index number (7 digits):",
-#             value=index_numbers
-#             # format='%d',
-#             # key=f"corrected_index_input_{student_id}"   # Using a key to track the session state of corrected_index
-#         )
-        
-        # Store the new value in a separate session state variable
-        # st.session_state[f"stored_corrected_index_{student_id}"] = corrected_index
-        # new_results=corrected_index
-        # if corrected_index:
-        #     st.session_state[f"corrected_index_input_{student_id}"]=new_results
-            # return corrected_index
-#         if f"corrected_index_input_{student_id}" in st.session_state:
-#             return corrected_index
-# hi=show_warning_and_get_input(index_numbers=4567,student_id=789)
-# st.write(hi)
-# # def trial():
-# corrected_index = st.text_input(
-#     "Enter correct index number (7 digits):",
-#     value=234,
-#     key="ama"
-# )
-# yaw=corrected_index
-# st.write(yaw)
-#     return corrected_index
-# returned_value=trial()
-# st.write("Returned value:", returned_value)
-# Configure logging to display debug messages
-# logging.basicConfig(level=logging.DEBUG)
-
-# Provide the path to the image
-# image_path = r'C:\Users\Sam\AppData\Local\Temp\tmpp9xb9m8s.jpg'
-
-# # Display the image
-# try:
-#     pil_image = Image.open(image_path)
-#     st.image(pil_image, use_column_width=True)
-# except Exception as e:
-#     logging.error(f"An error occurred: {e}")
-# Later in your code, after the user has interacted with the widget, you can access the value like this
-# if corrected_index:
-#     # Use the value entered by the user
-#     st.write("User entered corrected index:", corrected_index)
-
-
-                # # Create a list of file names for the dropdown menu
-                # file_names = [os.path.basename(result["image_paths"]) for result in all_results]
-
-                # # Display the dropdown menu
-                # selected_file = st.sidebar.selectbox("Select Image File", file_names)
-
-                # # Find the index of the selected file in the list of file names
-                # selected_index = file_names.index(selected_file)
-
-                # # Display the corresponding image
-                # img = Image.open(all_results[selected_index]["image_paths"])
-                # st.image(img, caption="Uploaded Image", use_column_width=True)
-    # else:
-    #     st.write("Please upload and process the reference and student sheets first.")
-
-    # if st.sidebar.button('View Results'):
-    #     st.set_page_config(page_title="Test Scoring System - Results", layout="wide")
-
-    #     # Display the DataFrame on a separate page
-    #     st.write("## Results")
-    #     st.dataframe(df)
-
-    #     # Add a back arrow button to return to the home page
-    #     if st.button("⬅️ Back"):
-    #         st.set_page_config(page_title="Test Scoring System", layout="wide")
-    # Create a function for each menu option
-    # def option1():
-    #     st.write("This is Option 1")
-
-    # def option2():
-    #     st.write("This is Option 2")
-
-    # def option3():
-    #     st.write("This is Option 3")
-
-    # # Create the menu in the sidebar
-    # menu_selection = st.sidebar.button("Graded sheets")
-
-    # # Call the corresponding function based on the selected option
-    # if menu_selection == "Option 1":
-    #     option1()
-    # elif menu_selection == "Option 2":
-    #     option2()
-    # elif menu_selection == "Option 3":
-    #     option3()
 
 
 
